16ba805e92fdfba54581fa1cf754096ee8ed8e44
RingK27 <111756643+RingK27@users.noreply.github.com>
2022-09-01 20:00:03 +0200

    Finished grid

--- scripts.js
+++ scripts.js
@@ -1,23 +1,49 @@
 const gridContainer = document.querySelector('#grid-container');
 
-let gridSize = 32;
+let gridSize = 64;
 
 function makeColumns(gridsize) {
     for (let i = 0; i < gridSize; i++) {
         const column = document.createElement("div");    
-        column.classList.add("column");    
+        column.classList.add("column");
+        column.setAttribute("id", "column" + i);
+        column.setAttribute("draggable", "false"); 
+        let colId = column.getAttribute("id");    
         gridContainer.appendChild(column);
-        makeRows(gridSize, column);           
+        makeRows(gridSize, column, colId);           
     }
 }
 
-function makeRows(gridSize, column) {
+function makeRows(gridSize, column, colId) {
     for (let i = 0; i < gridSize; i++) {
         const row = document.createElement("div");    
-        row.classList.add("row");    
+        row.classList.add("row");
+        row.setAttribute("id", "row" + i + colId);
+        row.addEventListener("mouseover", changeColor);
+        row.setAttribute("draggable", "false");  
         column.appendChild(row);                  
     }
 }
 
+function changeColor() {    
+    let grid = document.getElementById("grid-container");   
+    grid.onmouseover = function(e) {
+        let cellNumber = e.target.id;
+        let cell = document.getElementById(cellNumber);
+        if (mouseDown) {
+            cell.style.backgroundColor = "black";
+        }        
+    }   
+}
 
-makeColumns(gridSize);   
+let mouseDown = 0;
+
+document.body.onmousedown = function() { 
+    ++mouseDown;
+  }
+  document.body.onmouseup = function() {
+    --mouseDown;
+  }
+
+
+makeColumns(gridSize);  
